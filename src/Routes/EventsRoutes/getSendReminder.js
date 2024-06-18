fe5588276d99d4568b3bebbc66d9935f30a5e169
carlavega13 <carlavega231323@gmail.com>
2024-06-18 01:42:24 -0300

    Final testing

--- src/Routes/EventsRoutes/getSendReminder.js
+++ src/Routes/EventsRoutes/getSendReminder.js
@@ -1,8 +1,12 @@
 const sendEventReminder = require("../../utils/sendEventReminder");
-
+const { User } = require("../../db");
 const getSendReminder = async (req, res) => {
   try {
-    await sendEventReminder();
+    const admin = await User.findOne({ where: { rol: "admin",id:req.params.id } });
+    if(!admin){
+      return res.status(404).json({ message: "You are not authorized to send reminders" });
+    }
+    await sendEventReminder(admin.email);
     res.status(200).json({ message: "Reminders sent" });
   } catch (error) {
     res.status(200).json({ message: "An error occurred", error: error.message });
